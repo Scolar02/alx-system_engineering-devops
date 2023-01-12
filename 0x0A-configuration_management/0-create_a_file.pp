fd7a3d64bd887bc4ec90819587afaac71345cc66
Scolar02 <njugunascolar83@gmail.com>
2023-01-12 04:54:39 -0800

    task 0

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,10 +1,12 @@
 # Using puppet, create a file with below specifications
 
-file { '/tmp/holberton':
+node default {
+  file { '/tmp/holberton':
   ensure  => file,
   path    => '/tmp/holberton',
   mode    => '0744',
   owner   => 'www-data',
   group   => 'www-data',
   content => 'I love Puppet'
+  }
 }
