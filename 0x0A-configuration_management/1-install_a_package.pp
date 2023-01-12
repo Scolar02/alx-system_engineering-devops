7781d7bd34711642ad71ea6a98501709db848a7e
Scolar02 <njugunascolar83@gmail.com>
2023-01-12 05:00:33 -0800

    task 1

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,8 @@
 # Using Puppet, install puppet-lint
 
-package { 'puppet-lint':
-  ensure   => '2.1.1',
-  provider => 'gem'
+node default {
+  package { 'flask':
+    ensure   => '2.1.0',
+    provider => 'pip3',
+  }
 }
