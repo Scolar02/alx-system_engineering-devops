e7c5875fe61855983ea9036d2cfe600dcb24ea62
Scolar02 <njugunascolar83@gmail.com>
2023-07-25 03:59:01 -0700

    task 1

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -3,21 +3,52 @@
 Request from API; Return TODO list  given employee id
 Export this data to CSV
 """
-import csv
-import requests
 import sys
+import requests
+import csv
 
+def get_employee_todo_progress(employee_id):
+    base_url = "https://jsonplaceholder.typicode.com/users"
+    todos_url = f"{base_url}/{employee_id}/todos"
+
+    response = requests.get(todos_url)
+
+    if response.status_code != 200:
+        print("Error: Unable to fetch data from the API.")
+        sys.exit(1)
+
+    todos = response.json()
+    employee_name = todos[0]["name"]
+    total_tasks = len(todos)
+    completed_tasks = sum(1 for todo in todos if todo["completed"])
+
+    return employee_name, completed_tasks, total_tasks, todos
+
+def save_to_csv(employee_id, employee_name, todos):
+    filename = f"{employee_id}.csv"
+
+    with open(filename, "w", newline="") as csvfile:
+        csv_writer = csv.writer(csvfile, delimiter=",")
+        csv_writer.writerow(["USER_ID", "USERNAME", "TASK_COMPLETED_STATUS", "TASK_TITLE"])
+
+        for todo in todos:
+            task_completed_status = "True" if todo["completed"] else "False"
+            csv_writer.writerow([employee_id, employee_name, task_completed_status, todo["title"]])
+
+def display_todo_progress(employee_id):
+    employee_name, completed_tasks, total_tasks, todos = get_employee_todo_progress(employee_id)
+
+    print(f"Employee {employee_name} is done with tasks({completed_tasks}/{total_tasks}):")
+    for title in [todo["title"] for todo in todos if todo["completed"]]:
+        print(f"\t{title}")
+
+    save_to_csv(employee_id, employee_name, todos)
 
 if __name__ == "__main__":
-    root = "https://jsonplaceholder.typicode.com"
-    users = requests.get(root + "/users", params={"id": sys.argv[1]})
-    for names in users.json():
-        usr_id = names.get('id')
-        todo = requests.get(root + "/todos", params={"userId": usr_id})
-        csv_arr = []
-        with open(sys.argv[1] + ".csv", 'a') as f:
-            writer = csv.writer(f, quoting=csv.QUOTE_ALL)
-            for tasks in todo.json():
-                writer.writerow([names.get('id'), names.get('name'),
-                                 str(tasks.get('completed')),
-                                 tasks.get('title')])
+    if len(sys.argv) != 2:
+        print("Usage: python3 export_to_CSV.py <employee_id>")
+        sys.exit(1)
+
+    employee_id = int(sys.argv[1])
+    display_todo_progress(employee_id)
+
